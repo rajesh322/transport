8e0976e7959909569a1963bc695737b42f44622c
tejasvinu <tejasvinu@gmail.com>
2022-11-16 03:46:02 +0530

    Added intent to navigate outside the map in google maps.

--- app/src/main/java/com/example/transpo_2/MapsActivity.kt
+++ app/src/main/java/com/example/transpo_2/MapsActivity.kt
@@ -3,12 +3,15 @@ package com.example.transpo_2
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.transpo_2.databinding.ActivityMapsBinding
@@ -22,13 +25,14 @@ import com.google.android.libraries.places.api.Places
 import com.google.android.material.textfield.TextInputLayout
 import java.util.*
 
-
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private var userLat:Double =0.0
     private var userLong:Double=0.0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -90,13 +94,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n")
                 }
                 strAdd = strReturnedAddress.toString()
-                Log.w("My Current loction address", strReturnedAddress.toString())
+                Log.w("My Current location address", strReturnedAddress.toString())
             } else {
-                Log.w("My Current loction address", "No Address returned!")
+                Log.w("My Current location address", "No Address returned!")
             }
         } catch (e: Exception) {
             e.printStackTrace()
-            Log.w("My Current loction address", "Canont get Address!")
+            Log.w("My Current location address", "Cannot get Address!")
         }
         return strAdd
     }
@@ -118,7 +122,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         parent.editText?.setText(stradd)
         // Add a marker in Sydney and move the camera
         val userLoc = LatLng(userLat, userLong)
+        val Des = LatLng(12.9781739,77.5695603)
         mMap.addMarker(MarkerOptions().position(userLoc).title("You are Here"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(userLoc))
+
+        val elevatedButton = findViewById<Button>(R.id.elevatedButton)
+        elevatedButton.setOnClickListener {
+        val gmmIntentUri =
+            Uri.parse("google.navigation:q=${Des.latitude},${Des.longitude}")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
+        }
     }
 }
+
